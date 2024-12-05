f69b17088887fcfaba21c4a012f85ffd65a7274a
ot-o00 <faustineotavio@gmail.com>
2024-12-05 09:53:25 -0300

    aula02

---
+++ aula02.py
@@ -0,0 +1,10 @@
+print('no one mourns the wicked, good news!') 
+
+nome_usuario = 'Oto' # tipo -> str = string
+idade_usuario = 19 # tipo -> int = interger
+altura_usuario = 1.75 #tipo -> float = ponto flutuante
+maior_de_idade = True  #tipo -> bool = boolean
+print ('seja bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('vocÊ tem', altura_usuario, 'metros.')
+print('você é maior de idade?', maior_idade)
